a3a4387c30747704831718792715d2f9fafa8903
Mitsugu Sueyoshi <42401026+MitsuguSueyoshi@users.noreply.github.com>
2024-04-20 12:15:00 +0900

    自動生成できた

--- scripts/buf.sh
+++ scripts/buf.sh
@@ -1,31 +1,11 @@
 #!/bin/sh
 
-call_buf_generate() {
-  buf generate "$@" || exit $?
-}
-
-# 開始時刻を保存
-touch -t $(date +%Y%m%d%H%M.%S) /tmp/buf-start-timestamp
-
-##
-# start proto/source
-##
 cd proto
 buf lint || exit $?
 
-# api
-call_buf_generate --template buf.gen.yaml
+buf generate --template buf.gen.yaml
 
-
-cd - > /dev/null
-##
-# end proto/source
-##
-
-# 古いファイルの削除
-find proto/generated/ -name '*.gen.proto' ! -newer /tmp/buf-start-timestamp -exec rm {} \;
-
-# 古いファイルの削除
-find pkg/ \( -name '*.pb.go' -o -name '*.pb.validate.go' -o -name '*.connect.go' -o -name '*.gen.go' \) ! -newer /tmp/buf-start-timestamp -exec rm {} \;
+cd ../pkg
 
 buf format -w pb
+
